554d8dbe8a890dc4c611523a55e00bcc7132acea
Kyle Gentle <kylegentle@gmail.com>
2018-05-14 18:07:12 -0400

    Clean up Poloniex class

--- poloniex.py
+++ poloniex.py
@@ -1,6 +1,5 @@
 import aiohttp
 from datetime import datetime
-import logging
 
 from exchange import Exchange
 
@@ -18,43 +17,41 @@ class Poloniex(Exchange):
                 try:
                     assert resp.status == 200
                     response_dict = await resp.json()
-                    self.currencies = self._read_currency_data(response_dict)
+                    self.currencies = self._build_currency_dict(response_dict)
                 except AssertionError:
                     raise Exception(f'Bad response code {resp.status} '
                                     f'from {resp.url}')
 
-    def _read_currency_data(self, response_dict):
+    def _build_currency_dict(self, response_dict):
         currencies = dict()
         for coin, data in response_dict.items():
-            currencies[data['id']] = {
-                'name': data['name'],
-                'tx_fee': data['txFee'],
-            }
+            coin_id = data['id']
+            currencies[coin_id] = {'name': data['name'],
+                                   'tx_fee': data['txFee'], }
         return currencies
 
     @Exchange.async_static_rate_limit
     async def _get_ticker(self, pair):
-        pair = self._normalize(pair)
+        pair = self._normalize_pair(pair)
         params = {'command': 'returnTicker'}
+
         async with aiohttp.ClientSession() as session:
             async with session.get(self.endpoint, params=params) as resp:
                 try:
                     assert resp.status == 200
                     resp_dict = await(resp.json())
                     resp_ticker = resp_dict[pair]
-                    ticker = {
-                        'timestamp': datetime.now(),
-                        'exchange': self.name,
-                        'pair': pair,
-                        'bid': resp_ticker['highestBid'],
-                        'ask': resp_ticker['lowestAsk'],
-                        'last': resp_ticker['last'],
-                    }
+                    ticker = {'timestamp': datetime.now(),
+                              'exchange': self.name,
+                              'pair': pair,
+                              'bid': resp_ticker['highestBid'],
+                              'ask': resp_ticker['lowestAsk'],
+                              'last': resp_ticker['last'], }
                     return ticker
                 except AssertionError:
                     raise Exception(f'Bad response code {resp.status} '
                                     f'from {resp.url}')
 
     @staticmethod
-    def _normalize(pair):
+    def _normalize_pair(pair):
         return '_'.join(pair.split('-'))
