921cb324af6b84aaf47ea88e453f856dee1b8444
Kyle Gentle <kylegentle@gmail.com>
2018-06-06 21:46:34 -0400

    Add stream top-level function

---
+++ tickerqueue/stream.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+import uvloop
+
+from .exchanges import all_exchanges
+
+
+def stream(pair, exchanges=all_exchanges()):
+    logging.basicConfig(level=logging.INFO)
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    exchanges = initialize_exchanges(loop, *exchanges)
+    exchanges = [ex for ex in exchanges if ex.has_pair(pair.upper())]
+    ticker_stream_tasks = [
+        asyncio.ensure_future(ex.stream_ticker(pair.upper())) for ex in exchanges
+    ]
+    loop.run_until_complete(asyncio.gather(*ticker_stream_tasks))
+
+
+def initialize_exchanges(loop, *exchange_classes):
+    creation_tasks = [
+        asyncio.ensure_future(ex.create(report_coro=stream_to_stdout))
+        for ex in exchange_classes
+    ]
+    exchanges = loop.run_until_complete(asyncio.gather(*creation_tasks))
+    return exchanges
+
+
+async def stream_to_stdout(self, pair):
+    while True:
+        ticker = await self._get_ticker(pair)
+        foo = "    ".join([f"{k}={v}" for k, v in ticker.items()])
+        print(foo)
