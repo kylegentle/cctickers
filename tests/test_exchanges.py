8f4c3963b0aeeba109598e5d86da6b606a07b3b7
Kyle Gentle <kylegentle@gmail.com>
2018-06-15 22:43:35 -0400

    Parametrize get_ticker test, add pytest-asyncio to dependencies

--- tests/test_exchanges.py
+++ tests/test_exchanges.py
@@ -10,15 +10,19 @@ from tickerqueue.exchanges import all_exchanges
 EXCHANGE_CLASSES = all_exchanges()
 
 
+# TODO: Refactor for pytest-asyncio?
 class TestExchange:
 
     @pytest.fixture(scope="module")
     def loop(self):
-        return asyncio.get_event_loop()
+        loop = asyncio.get_event_loop()
+        yield loop
+        loop.close()
 
     @pytest.fixture(scope="module", params=EXCHANGE_CLASSES)
     def exchange(self, loop, request):
-        setup_task = asyncio.ensure_future(request.param.create())
+        exchange_cls = request.param
+        setup_task = asyncio.ensure_future(exchange_cls.create())
         exchange = loop.run_until_complete(setup_task)
         yield exchange
         teardown_task = asyncio.ensure_future(exchange.session.close())
@@ -28,17 +32,29 @@ class TestExchange:
         assert exchange.connected is True
         assert isinstance(exchange.session, aiohttp.ClientSession)
 
-    def test_get_ticker(self, loop, exchange):
+    @pytest.fixture
+    def ticker(self, exchange, loop):
         pair = "BTC-ETH"
         ticker_task = asyncio.ensure_future(exchange._get_ticker(pair))
         ticker = loop.run_until_complete(ticker_task)
-        assert isinstance(ticker["timestamp"], datetime)
-        assert ticker["exchange"] is not None
-        assert isinstance(ticker["bid"], Decimal)
-        assert isinstance(ticker["ask"], Decimal)
-        assert isinstance(ticker["last"], Decimal) or ticker["last"] is None
+        return ticker
 
-    def test_has_pairs(self, exchange):
-        assert exchange.has_pair("BTC-ETH")
-        assert exchange.has_pair("BTC-XRP")
-        assert exchange.has_pair("BTC-XMR")
+    @pytest.mark.parametrize(
+        "key,key_type",
+        [
+            ("timestamp", datetime),
+            ("exchange", str),
+            ("bid", Decimal),
+            ("ask", Decimal),
+            ("last", Decimal),
+        ],
+    )
+    def test_get_ticker(self, ticker, key, key_type):
+        if key == "last":
+            assert isinstance(ticker[key], key_type) or ticker[key] is None
+        else:
+            assert isinstance(ticker[key], key_type)
+
+    @pytest.mark.parametrize("pair", ["BTC-ETH", "BTC-XRP", "BTC-XMR", "BTC-LTC"])
+    def test_has_pair(self, exchange, pair):
+        assert exchange.has_pair(pair)
